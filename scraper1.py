21b561dfc0bdb73162a79ebbccdda268de83a656
testtest137 <142800737+testtest137@users.noreply.github.com>
2024-07-15 15:09:53 +0530

    Update scraper1.py

--- scraper1.py
+++ scraper1.py
@@ -101,7 +101,7 @@ def startScrape(contest_name):
     driver.quit()
     
     temp = open('err.html', 'w')
-    temp.write(soup.prettify("utf-8"))
+    temp.write(unicode(soup))
     temp.close()
     print(soup)
 
