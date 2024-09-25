f6dab597d049838758bd4e8f20dc04ae4470a154
Shashi-Preetam-test <142800737+Shashi-Preetam-test@users.noreply.github.com>
2024-09-25 16:59:03 +0530

    Update scraper1.py

--- scraper1.py
+++ scraper1.py
@@ -1,118 +1,50 @@
-import sys, json
+import os, sys
 from selenium import webdriver
-import chromedriver_autoinstaller
-from pyvirtualdisplay import Display
+from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
+from subprocess import Popen, CREATE_NEW_CONSOLE
 from pymongo import MongoClient
 
-display = Display(visible=0, size=(800, 800))  
-display.start()
+# concurrent_windows = 10
 
-chromedriver_autoinstaller.install()
-
-chrome_options = webdriver.ChromeOptions()    
-
+chrome_options = Options()
 options = [
-  # Define window size here
-   "--window-size=800,800",
-    "--ignore-certificate-errors"
-    "--headless",
-    
-    #"--disable-gpu",
-    #"--disable-extensions",
-    #"--no-sandbox",
-    #"--disable-dev-shm-usage",
-    #'--remote-debugging-port=9222'
+    # "--headless",
+    # "--disable-gpu",
+    # "--window-size=0,0",
+    "--ignore-certificate-errors",
+    "--disable-extensions",
+    "--no-sandbox",
+    # "--disable-dev-shm-usage",
+    # "--remote-debugging-pipe"
 ]
-
 for option in options:
     chrome_options.add_argument(option)
-    
-client = MongoClient("mongodb+srv://shashi:shashi123@leetcode-rankify.kno7lqv.mongodb.net/?retryWrites=true&w=majority&appName=LeetCode-Rankify")
+
+mongodb_username = os.getenv('MONGODB_USERNAME')
+mongodb_password = os.getenv('MONGODB_PASSWORD')
+cluster_url = os.getenv('MONGODB_CLUSTER_URL')
+
+client = MongoClient(f"mongodb+srv://{mongodb_username}:{mongodb_password}@{cluster_url}/?retryWrites=true&w=majority&appName=LeetCode-Rankify")
 db = client["LeetCode-Rankify"]
 
-def getResults(contest_name, pages):
-    url = "https://leetcode.com/contest/{}/ranking/".format(contest_name)
-    page = 1
-
-    results = []
-
-    client = MongoClient("mongodb+srv://shashi:shashi123@leetcode-rankify.kno7lqv.mongodb.net/?retryWrites=true&w=majority&appName=LeetCode-Rankify")
-    db = client["LeetCode-Rankify"]
-            
-    while(page <= pages):
-        driver = webdriver.Chrome(options=chrome_options)
-            
-        try:
-            driver.get(url + str(page))
-        except Exception as e:
-            print("Couldn't fetch page {}.".format(page), e, "\nRetrying.")
-            driver.quit()
-            continue
-
-        soup = BeautifulSoup(driver.page_source, 'html.parser')
-
-        if(len(soup.find_all('tr')) <= 1):
-            print("Couldn't fetch page {}. Retying.".format(page))
-            continue
-      
-        for row in soup.find_all('tr'):
-            if row.find(class_="ranking-username") is None:
-                continue
-
-            user = dict()
-
-            ptr = row.find('td')
-            user["rank"] = int(ptr.get_text().strip())
-
-            ptr = ptr.find_next('td')
-            user["name"] = ptr.find('a')["title"]
-
-            ptr = ptr.find_next('td')
-            user["score"] = int(ptr.get_text().strip())
-
-            ptr = ptr.find_next('td')
-            user["finish_time"] = ptr.get_text().strip()
-          
-            results.append(user)
-        
-        driver.quit()
-
-        print("Results fetched from page {}.".format(page))
-        page += 1
-    
-    db[contest_name].insert_many(results)
-    print("Results fetched from pages {} to {}.".format(1, pages))
-
-
-def startScrape(contest_name):
+def getResults(contest_name):
     url = "https://leetcode.com/contest/" + contest_name + "/ranking"
+    
+    driver = webdriver.Chrome(options=chrome_options)
+    
+    driver.get(url)
+    
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    
+    driver.quit()
+    
+    pages = int(soup.find_all(class_ = "page-btn")[-1].get_text())
+    
+    Popen(('python scraper-worker.py {} {} {}'.format(contest_name, 1, pages)), creationflags=CREATE_NEW_CONSOLE)
+    
 
-    while(True):
-      driver = webdriver.Chrome(options=chrome_options)
-      
-      try:
-          driver.get(url)
-      except Exception as e:
-          print("Couldn't fetch number of pages.", e, "\nRetrying.")
-          driver.quit()
-          continue
-      
-      soup = BeautifulSoup(driver.page_source, 'html.parser')
-      
-      driver.quit()
-  
-      if(len(soup.find_all(class_ = "page-btn")) == 0):
-          print("Couldn't fetch number of pages. Retrying.")
-          continue
-        
-      pages = int(soup.find_all(class_ = "page-btn")[-1].get_text())
-  
-      print("{} pages found.".format(pages))
-      getResults(contest_name, pages)
-      break
-
-if len(sys.argv) != 2 or sys.argv[1] == "":
+if len(sys.argv) != 2:
     url = "https://leetcode.com/contest/"
     
     driver = webdriver.Chrome(options=chrome_options)
@@ -129,4 +61,4 @@ else:
     contest_name = sys.argv[1]
 
 print(contest_name)
-startScrape(contest_name=contest_name)
+getResults(contest_name=contest_name)
